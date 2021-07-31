00e54b2feadc44dacd93655926212f7cc3b74497
DEUK CHEON, OH <guruwang2@gmail.com>
2021-07-31 14:46:19 +0900

    행 너비 수정 구현 완료

--- pages/test/movingborder/index.tsx
+++ pages/test/movingborder/index.tsx
@@ -1,85 +1,104 @@
 import React, { useEffect, useState } from 'react';
 import styles from './movingborder.module.scss';
 import Image from 'next/image';
+import {
+  atom,
+  selector,
+  useRecoilState,
+  useRecoilValue,
+  useSetRecoilState
+} from 'recoil';
 
-const HeaderElement = ({
-  store,
-  value,
-  id
-}: {
-  store: DragStore;
-  value: number;
-  id: any;
-}) => {
+const HeaderElement = ({ value, id }: { value: number; id: any }) => {
+  const widthList = useRecoilValue(widthListState);
+  const setWidthList = useSetRecoilState(widthListState);
   const [backgroundColor, setBackgroundColor] = useState('white');
   const [width, setWidth] = useState(10);
   const [state, setState] = useState({
     preWidth: width
   });
-  useEffect(() => {
-    store.setObject(id, {
-      onMouseDown: () => {
-        setBackgroundColor('yellow');
-      },
-      onMouseMove: (posGap: any) => {
-        setWidth(state.preWidth + posGap.x);
-      },
-      onMouseUp: () => {
-        setBackgroundColor('white');
-        state.preWidth = width;
-      }
-    });
-  }, [width]);
-
+  console.log('id : ', id);
   return (
     <>
       <div
-        draggable={false}
         className={styles.headerContent}
         style={{ backgroundColor: backgroundColor, width: width }}
       >
         {value}
+
+        <Draggable
+          id={id}
+          observer={width}
+          onMouseDown={() => {
+            setBackgroundColor('yellow');
+          }}
+          onMouseMove={(posGap: any) => {
+            const width = state.preWidth + posGap.x;
+            setWidth(width);
+            const array: any[] = widthList.slice();
+            array[id] = width;
+            setWidthList(array);
+          }}
+          onMouseUp={() => {
+            setBackgroundColor('white');
+            state.preWidth = width;
+          }}
+        >
+          <div className={styles.drag} />
+        </Draggable>
       </div>
-      <div
-        draggable={false}
-        className={styles.drag}
-        onMouseDown={() => {
-          store.curKey = id;
-          store.onMouseDown();
-        }}
-        onBlur={() => {
-          state.on = false;
-          state.preWidth = width;
-        }}
-      />
     </>
   );
 };
 
 type Props = {};
-
-const DragArea = ({ children, store }: { children: any; store: DragStore }) => {
-  const [state, setState] = useState({
-    on: false,
-    prePosX: 0
+const Draggable = ({
+  id,
+  observer,
+  onMouseDown,
+  onMouseUp,
+  onMouseMove,
+  children
+}: any) => {
+  useEffect(() => {
+    store.setObject(id, {
+      onMouseDown: () => {
+        onMouseDown(id);
+      },
+      onMouseMove: (posGap: any) => {
+        onMouseMove(posGap);
+      },
+      onMouseUp: () => {
+        onMouseUp();
+      }
+    });
   });
   return (
     <div
+      onMouseDownCapture={() => {
+        store.onMouseDown(id);
+      }}
+      className={styles.draggable}
+    >
+      {children}
+    </div>
+  );
+};
+const DragArea = ({ children }: { children: any; store: DragStore }) => {
+  return (
+    <div
       onMouseDownCapture={e => {
-        state.on = true;
-        state.prePosX = e.clientX;
-        store.onMouseDown();
+        store.state.prePosX = e.clientX;
       }}
       onMouseMove={e => {
-        if (!state.on) return;
+        if (!store.state.on) return;
         const gapPos = {
-          x: e.clientX - state.prePosX
+          x: e.clientX - store.state.prePosX
         };
-        store.onMouseMove(gapPos);
+        const array = store.onMouseMove(gapPos);
       }}
       onMouseUpCapture={e => {
-        if (!state.on) return;
-        state.on = false;
+        if (!store.state.on) return;
         store.onMouseUp();
       }}
     >
@@ -98,9 +117,12 @@ interface DragObject {
 
 class DragStore {
   objectList: DragObject = {};
-  curKey: string | number = 0;
+  curKey: string | number = -1;
+  state: { on: boolean; prePosX: number } = { on: false, prePosX: 0 };
 
-  onMouseDown = () => {
+  onMouseDown = (id?: any) => {
+    this.state.on = true;
+    if (id != null) this.curKey = id;
     this.objectList[this.curKey]?.onMouseDown();
   };
   onMouseMove = (gapPos: any) => {
@@ -108,7 +130,8 @@ class DragStore {
   };
   onMouseUp = () => {
     this.objectList[this.curKey]?.onMouseUp();
-    this.curKey = 0;
+    this.curKey = -1;
+    this.state.on = false;
   };
   setObject = (key: string, object: any) => {
     const obj: { [key: string]: any } = {};
@@ -117,28 +140,62 @@ class DragStore {
   };
 }
 
+export const store = new DragStore();
+
+const widthListState = atom({
+  key: 'widthList',
+  default: []
+});
+const widthListSelector = selector({
+  key: 'widthListSelector',
+  get: ({ get }) => {
+    return get(widthListState);
+  }
+});
+
 const index = (props: Props) => {
-  useEffect(() => {}, []);
-  const dragStore = new DragStore();
+  const setWidthList = useSetRecoilState(widthListState);
+  const widthList = useRecoilValue(widthListState);
+  useEffect(() => {
+    const array = Array.apply(null, new Array(5)).map(
+      Number.prototype.valueOf,
+      10
+    );
+    setWidthList(array);
+  }, []);
+  console.log(widthList);
+  const headers = [1, 2, 3, 4, 5];
+  const rowList = { 0: [1, 2, 3, 4, 5], 1: [7, 8, 9, 10, 11] };
   return (
     <>
-      <DragArea store={dragStore}>
+      <DragArea store={store}>
         <div style={{ height: '100vh' }}>
-          <div className={`${styles.table} ${styles.header}`}>
-            <HeaderElement
-              store={dragStore}
-              value={1}
-              key={1}
-              id={1}
-            ></HeaderElement>
-            <HeaderElement
-              store={dragStore}
-              value={2}
-              key={2}
-              id={2}
-            ></HeaderElement>
-            {/* <HeaderElement value={2} key={2}></HeaderElement> */}
+          <div className={`${styles.header}`}>
+            {headers.map((header, index: number) => {
+              return (
+                <HeaderElement
+                  value={header}
+                  key={index}
+                  id={index}
+                ></HeaderElement>
+              );
+            })}
           </div>
+          {Object.values(rowList).map((row, index) => {
+            return (
+              <>
+                <div className={styles.row}>
+                  {row.map((one, index: number) => {
+                    return (
+                      <div key={index} style={{ width: widthList[index] }}>
+                        {one}
+                      </div>
+                    );
+                  })}
+                </div>
+              </>
+            );
+          })}
         </div>
       </DragArea>
     </>
