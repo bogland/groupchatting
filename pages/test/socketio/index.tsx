24d4f5a523022ccc25f7c5ab189de8e184fe9d0e
DEUK CHEON, OH <guruwang2@gmail.com>
2021-07-31 22:23:57 +0900

    socketio 테스트 완료

--- pages/test/socketio/index.tsx
+++ pages/test/socketio/index.tsx
@@ -1,23 +1,27 @@
-import React, { useEffect, useState } from "react";
-import io from "socket.io-client";
+import React, { useEffect, useState } from 'react';
+import io from 'socket.io-client';
 
 type Props = {};
-const socket = io("http://localhost:5000");
 
 const index = (props: Props) => {
+  const [socket, setSocket] = useState<any>();
   useEffect(() => {
-    socket.on("connect", () => {
-      console.log("connected");
-    });
-
-    socket.on("chat", (data) => {
+    const socket = io('http://192.168.0.2:5000');
+    setSocket(socket);
+    socket.emit('join', 'join');
+    socket.on('chat', data => {
+      alert(data);
       console.log(data);
     });
+
+    return () => {
+      socket.disconnect();
+    };
   }, []);
 
   const sendMessage = () => {
     // console.log("gdgd");
-    socket.emit("chat", "ㅎㅇㅎㅇ");
+    socket.emit('chat', 'ㅎㅇㅎㅇ');
     return () => socket.disconnect();
   };
 
